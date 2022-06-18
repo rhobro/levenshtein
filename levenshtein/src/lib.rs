f1b3227c66df6c5d47fdfc818b3d1939e0511c20
Rohan Mathew <robozmat@gmail.com>
2022-06-18 20:55:05 +0100

    non ndarray

--- levenshtein/src/lib.rs
+++ levenshtein/src/lib.rs
@@ -1,5 +1,4 @@
 use wasm_bindgen::prelude::*;
-use ndarray::prelude::*;
 
 #[wasm_bindgen]
 pub fn measure(a: &str, b: &str) -> f64 {
@@ -7,22 +6,42 @@ pub fn measure(a: &str, b: &str) -> f64 {
     let b = b.to_lowercase();
 
     // initialize matrix
-    let mut matrix: Array<f64, _> = Array::zeros((a.len()+1, b.len()+1));
-    matrix.column_mut(0).assign(&Array::range(0., (a.len() + 1) as f64, 1.));
-    matrix.row_mut(0).assign(&Array::range(0., (b.len() + 1) as f64, 1.));
+    let mut matrix = gen_matrix(a.len(), b.len());
 
     // loop over letters
     for (i, aa) in (1..a.len()+1).zip(a.chars()) {
         for (j, bb) in (1..b.len()+1).zip(b.chars()) {
             let sub_cost = if aa == bb { 0. } else { 1. };
 
-            matrix[[i, j]] = [
-                matrix[[i-1, j]] + 1.,
-                matrix[[i, j-1]] + 1.,
-                matrix[[i-1, j-1]] + sub_cost,
+            matrix[i][j] = [
+                matrix[i-1][j] + 1.,
+                matrix[i][j-1] + 1.,
+                matrix[i-1][j-1] + sub_cost,
             ].into_iter().min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
         }
     }
 
-    matrix[[a.len()-1, b.len()-1]]
+    matrix[a.len()-1][b.len()-1]
+}
+
+fn gen_matrix(a: usize, b: usize) -> Vec<Vec<f64>> {
+    let mut matrix = vec![];
+    for i in 0..a+1 {
+        matrix.push({
+            let mut row = vec![];
+            for i in 0..b+1 {
+                row.push(0.);
+            }
+            row
+        });
+    }
+    
+    for i in 1..a+1 {
+        matrix[i][0] = (i) as f64;
+    }
+    for i in 1..b+1 {
+        matrix[0][i] = (i) as f64;
+    }
+
+    matrix
 }
